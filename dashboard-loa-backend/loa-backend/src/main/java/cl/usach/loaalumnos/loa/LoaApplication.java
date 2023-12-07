fdb0db392b5ea35d226ec33b8032e2381ac3b37a
Altobert <alberto.sanmartin@usach.cl>
2023-12-07 17:27:24 -0300

    fuentes backend

---
+++ dashboard-loa-backend/loa-backend/src/main/java/cl/usach/loaalumnos/loa/LoaApplication.java
@@ -0,0 +1,13 @@
+package cl.usach.loaalumnos.loa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoaApplication.class, args);
+	}
+
+}
---
+++ dashboard-loa-backend/loa-backend/src/test/java/cl/usach/loaalumnos/loa/LoaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usach.loaalumnos.loa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
